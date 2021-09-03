f643129b2087442367a09b83c7c31f2177a8bb5f
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2021-09-03 13:31:31 +0100

    Real time update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+
+  const now = new Date().toLocaleTimeString();
+  const [time,setTime ] = useState(now)
+
+  function updateTime(){
+    const newTime = new Date.toLocaleTimeString();
+    setTime(newTime)
+  }
+
+
+
+  function realTimeUpdate() {
+    const realTime = new Date().toLocaleTimeString();
+       setTime(realTime)
+      
+   }
+
+
+  setInterval(realTimeUpdate, 1000);
+ 
+
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
